e4cf267c397b876e80dd21a49f8dad37c29709d8
niat786 <niat786@gmail.com>
2025-09-24 22:54:54 +0500

    page speed and analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -40,6 +42,8 @@ export default function RootLayout({
         className={` antialiased`}
       >
         {children}
+        <SpeedInsights/>
+        <Analytics/>
       </body>
     </html>
   );
